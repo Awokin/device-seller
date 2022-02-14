8e9545c07b2fc12105414947f6b93ccd2461f27e
Awoseemo Olaposi <akinlolu.awoseemo@upperlink.ng>
2022-02-14 03:55:29 +0100

    Added Cross Origin

--- src/main/java/com/sha/deviceseller/controller/PurchaseController.java
+++ src/main/java/com/sha/deviceseller/controller/PurchaseController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/purchase")
 public class PurchaseController {
 
--- src/main/java/com/sha/deviceseller/controller/UserController.java
+++ src/main/java/com/sha/deviceseller/controller/UserController.java
@@ -6,12 +6,10 @@ import com.sha.deviceseller.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/user")
 public class UserController {
 
